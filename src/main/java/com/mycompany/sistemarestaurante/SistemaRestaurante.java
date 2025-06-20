ec1a2a449fa692ffe7c4a75b2d940953016611ed
Nicolás Moreno <nmp0617@gmail.com>
2025-06-20 11:57:53 -0500

    login terminado

--- src/main/java/com/mycompany/sistemarestaurante/SistemaRestaurante.java
+++ src/main/java/com/mycompany/sistemarestaurante/SistemaRestaurante.java
@@ -1,15 +1,14 @@
 
 package com.mycompany.sistemarestaurante;
 
-import com.mycompany.sistemarestaurante.model.*;
-import com.mycompany.sistemarestaurante.crud.*;
+
 import com.mycompany.sistemarestaurante.views.*;
 
 
 public class SistemaRestaurante {
 
     public static void main(String[] args) {
-        FormUsers form = new FormUsers();
+        Login form = new Login();
         form.setVisible(true);
         form.setLocationRelativeTo(null);
         
